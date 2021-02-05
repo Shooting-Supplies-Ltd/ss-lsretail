13a3cf68c8ca4d58edf720ba70147f68da14a6ab
Darryl Morley <darrylmorley.uk@gmail.com>
2021-02-05 15:37:06 +0000

    Add await to ls api calls

--- pages/api/lightspeed.js
+++ pages/api/lightspeed.js
@@ -19,56 +19,56 @@ async function getHeader() {
 
 export async function getItems() {
   let axiosConfig = await getHeader()
-  return axios.get(`Item.json?load_relations=["Category", "Images", "ItemShops", "CustomFieldValues"]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
+  return await axios.get(`Item.json?load_relations=["Category", "Images", "ItemShops", "CustomFieldValues"]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getItem(itemID) {
   let axiosConfig = await getHeader()
-  return axios.get(`Item/${itemID}.json?load_relations=["Category", "Images", "ItemShops", "CustomFieldValues", "ItemECommerce"]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
+  return await axios.get(`Item/${itemID}.json?load_relations=["Category", "Images", "ItemShops", "CustomFieldValues", "ItemECommerce"]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getAmmo() {
   let axiosConfig = await getHeader()
-  return axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[23,46,218]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
+  return await axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[23,46,218]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getAccessories() {
   let axiosConfig = await getHeader()
-  return axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[77, 153, 73, 2, 262, 163, 21, 78, 261, 24, 263, 76, 166, 212, 56, 59, 42, 103, 104, 236, 66, 145, 207]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
+  return await axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[77, 153, 73, 2, 262, 163, 21, 78, 261, 24, 263, 76, 166, 212, 56, 59, 42, 103, 104, 236, 66, 145, 207]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getOptics() {
   let axiosConfig = await getHeader()
-  return axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[39, 109, 91, 105, 267, 4, 36, 27, 37, 80, 29, 16, 150, 266, 259]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
+  return await axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[39, 109, 91, 105, 267, 4, 36, 27, 37, 80, 29, 16, 150, 266, 259]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getSecurity() {
   let axiosConfig = await getHeader()
-  return axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[206, 124, 131]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
+  return await axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[206, 124, 131]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getMaintenance() {
   let axiosConfig = await getHeader()
-  return axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[164, 52, 106, 86, 57, 14, 58, 65, 96, 83, 245, 165]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
+  return await axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[164, 52, 106, 86, 57, 14, 58, 65, 96, 83, 245, 165]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getClothing() {
   let axiosConfig = await getHeader()
-  return axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[5]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
+  return await axios.get(`Item.json?load_relations=["ItemShops", "Category", "Images", "ItemECommerce"]&ItemShops.qoh=%3E%3D,1&Category.categoryID=IN,[5]&ItemECommerce.listOnStore=true`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getCategories(categoryID) {
   let axiosConfig = await getHeader()
-  return axios.get(`Category.json?categoryID=IN,${categoryID}`, axiosConfig).catch(err => console.error(err.data))
+  return await axios.get(`Category.json?categoryID=IN,${categoryID}`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getManufacturers(manufacturerID) {
   let axiosConfig = await getHeader()
-  return axios.get(`Manufacturer.json?manufacturerID=IN,${manufacturerID}`, axiosConfig).catch(err => console.error(err.data))
+  return await axios.get(`Manufacturer.json?manufacturerID=IN,${manufacturerID}`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function createSale(newSale) {
   let axiosConfig = await getHeader()
-  return axios.post(`Sale.json`, newSale, axiosConfig).catch(error => console.error(error.data).then(res => { return res })).catch(err => console.error(err.data))
+  return await axios.post(`Sale.json`, newSale, axiosConfig).catch(error => console.error(error.data).then(res => { return res })).catch(err => console.error(err.data))
 }
 

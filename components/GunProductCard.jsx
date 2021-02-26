a9b5c144b7b7b5a847ed2096da60e57fc001745c
Darryl Morley <darrylmorley.uk@gmail.com>
2021-02-26 17:22:45 +0000

    fix alt tag

--- components/GunProductCard.jsx
+++ components/GunProductCard.jsx
@@ -1,26 +1,32 @@
-import Link from 'next/link'
-import slugify from 'slugify'
+import Link from 'next/link';
+import slugify from 'slugify';
 
 const GunProductCard = ({ gun }) => {
-  let slug = slugify(`${gun.Make}-${gun.Model}-${gun.Variant}-${gun.SerialNumber}`).toLowerCase()
+  const slug = slugify(`${gun.Make}-${gun.Model}-${gun.Variant}-${gun.SerialNumber}`).toLowerCase();
 
   return (
     <>
       <div className="block h-80 border-2 border-gray-300 rounded-lg bg-ssblue" key={gun.SerialNumber}>
-        <Link href={`/guns/[slug]`} as={`/guns/${slug}`}>
+        <Link href="/guns/[slug]" as={`/guns/${slug}`}>
           <a>
             <div className="flex h-48 overflow-hidden">
-              <img src={gun.Images[0].FullPath} alt={`Photo of ${gun.Make} ${gun.Model} ${gun.Variant ? gun.Variant : ''}`} className="w-full object-cover object-center" />
+              <img
+                src={gun.Images[0].FullPath}
+                alt={`${gun.Make} ${gun.Model} ${gun.Variant ? gun.Variant : ''}`}
+                className="w-full object-cover object-center"
+              />
             </div>
             <div className="p-4 flex flex-col text-white hover:text-ssorange">
-              <h2 className="flex justify-center text-center uppercase">{`${gun.Make} ${gun.Model} ${gun.Variant ? gun.Variant : ''}`}</h2>
+              <h2 className="flex justify-center text-center uppercase">{`${gun.Make} ${gun.Model} ${
+                gun.Variant ? gun.Variant : ''
+              }`}</h2>
               <p className="mt-2 flex justify-center font-bold text-lg">£{gun.Price}</p>
             </div>
           </a>
         </Link>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default GunProductCard
+export default GunProductCard;

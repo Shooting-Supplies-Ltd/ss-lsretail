a685dbf96d06317a678524c492e06913a3fbffcd
Darryl Morley <darrylmorley.uk@gmail.com>
2021-02-26 12:25:06 +0000

    add pages

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,142 +1,107 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-const Footer = () => {
-  return (
-    <div className="h-72 bg-blue-900">
-      <footer className="mx-12 grid grid-cols-4 gap-8 ssblue">
-        <div className="my-12 flex flex-col">
-          <h3 className="mb-2 text-white font-bold text-xl">About Us</h3>
-          <ul className="text-white space-y-1">
-            <li>
-              <Link href="/about">
-                <a className="hover:text-ssorange">
-                  About Us
-              </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog">
-                <a className="hover:text-ssorange">
-                  Contact Us
-              </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog">
-                <a className="hover:text-ssorange">
-                  Privacy Policy
-              </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog">
-                <a className="hover:text-ssorange">
-                  Blog
-              </a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="my-12 flex flex-col">
-          <h3 className="mb-2 text-white font-bold text-xl">Delivery & Returns</h3>
-          <ul className="text-white space-y-1">
-            <li>
-              <Link href="/terms">
-                <a className="hover:text-ssorange">
-                  Terms & Conditions
-              </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/returns">
-                <a className="hover:text-ssorange">
-                  Returns Policy
-              </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/privacy">
-                <a className="hover:text-ssorange">
-                  Privacy Policy
-              </a>
-              </Link>
-            </li>
-            {/* <li>
+const Footer = () => (
+  <div className="h-72 bg-blue-900">
+    <footer className="mx-12 grid grid-cols-4 gap-8 ssblue">
+      <div className="my-12 flex flex-col">
+        <h3 className="mb-2 text-white font-bold text-xl">About Us</h3>
+        <ul className="text-white space-y-1">
+          <li>
+            <Link href="/about">
+              <a className="hover:text-ssorange">About Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a className="hover:text-ssorange">Contact Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              <a className="hover:text-ssorange">Blog</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="my-12 flex flex-col">
+        <h3 className="mb-2 text-white font-bold text-xl">Terms & Conditions</h3>
+        <ul className="text-white space-y-1">
+          <li>
+            <Link href="/terms">
+              <a className="hover:text-ssorange">Terms & Conditions</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/terms#cancellation">
+              <a className="hover:text-ssorange">Returns Policy</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/privacy">
+              <a className="hover:text-ssorange">Privacy Policy</a>
+            </Link>
+          </li>
+          {/* <li>
             <Link href="/blog">
               <a>
                 Blog
               </a>
             </Link>
           </li> */}
-          </ul>
-        </div>
-        <div className="my-12 flex flex-col">
-          <h3 className="mb-2 text-white font-bold text-xl">Customer Support</h3>
-          <ul className="text-white space-y-1">
-            <li>
-              <Link href="/customer-account">
-                <a className="hover:text-ssorange">
-                  Your Account
-              </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/clothing-sizes">
-                <a className="hover:text-ssorange">
-                  Clothing Sizes
-              </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/trade-enquiries">
-                <a className="hover:text-ssorange">
-                  Trade Enquiries
-              </a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="my-12 flex flex-col">
-          <h3 className="mb-2 text-white font-bold text-xl">More...</h3>
-          <ul className="text-white space-y-1">
-            <li>
-              <Link href="/finance">
-                <a className="hover:text-ssorange">
-                  Finance
-              </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/our-brands">
-                <a className="hover:text-ssorange">
-                  Our Brands
-              </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/gunsmithing">
-                <a className="hover:text-ssorange">
-                  Gunsmithing Service
-              </a>
-              </Link>
-            </li>
-            <li>
-              <a href="https://fabdefense.co.uk" target="_blank" className="hover:text-ssorange">
-                FAB Defense
-              </a>
-            </li>
-            <li>
-              <Link href="/blog">
-                <a className="hover:text-ssorange">
-                  Tippmann Arms
-              </a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </footer>
-    </div>
-  )
-}
+        </ul>
+      </div>
+      <div className="my-12 flex flex-col">
+        <h3 className="mb-2 text-white font-bold text-xl">Customer Support</h3>
+        <ul className="text-white space-y-1">
+          <li>
+            <Link href="/customer-account">
+              <a className="hover:text-ssorange">Your Account</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/clothing-sizes">
+              <a className="hover:text-ssorange">Clothing Sizes</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/trade-enquiries">
+              <a className="hover:text-ssorange">Trade Enquiries</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="my-12 flex flex-col">
+        <h3 className="mb-2 text-white font-bold text-xl">More...</h3>
+        <ul className="text-white space-y-1">
+          <li>
+            <Link href="/finance">
+              <a className="hover:text-ssorange">Finance</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/our-brands">
+              <a className="hover:text-ssorange">Our Brands</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/gunsmithing">
+              <a className="hover:text-ssorange">Gunsmithing Service</a>
+            </Link>
+          </li>
+          <li>
+            <a href="https://fabdefense.co.uk" target="_blank" className="hover:text-ssorange">
+              FAB Defense
+            </a>
+          </li>
+          <li>
+            <Link href="/blog">
+              <a className="hover:text-ssorange">Tippmann Arms</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  </div>
+);
 
-export default Footer
+export default Footer;
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,25 +1,82 @@
-import Layout from '../components/layout/Layout'
+import Layout from '../components/layout/Layout';
 
-const Contact = () => {
-  return (
-    <Layout>
-      <main className="my-12 mx-96 px-40">
-        <h2 className="mb-4 font-bold text-2xl">Contact Us</h2>
-        <p>Tel: 01527831261</p>
-        <p>Email: info@shootingsuppliesltd.co.uk</p>
-        <p>Address: 38, Sherwood Road, Bromsgrove, B60 3DR</p>
-        <hr className="my-4" />
-        <h3 className="mb-4 font-bold text-2xl">Opening Times</h3>
-        <p>Mon: Closed</p>
-        <p>Tue: 09:00 - 17:30</p>
-        <p>Wed: 09:00 - 17:30</p>
-        <p>Thu: 09:00 - 17:30</p>
-        <p>Fri: 09:00 - 19:00</p>
-        <p>Sat: 09:00 - 17:30</p>
-        <p>Sun: Closed</p>
-      </main>
-    </Layout>
-  )
-}
+const Contact = () => (
+  <Layout>
+    <>
+      <form className="mx-72 my-12">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
+              First Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-blue-100 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="first-name"
+              type="text"
+              placeholder="First"
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="last-name">
+              Last Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-blue-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="last-name"
+              type="text"
+              placeholder="Last"
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="appearance-none block w-full bg-blue-100 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="email"
+              type="text"
+              placeholder="Email"
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="phone">
+              Phone
+            </label>
+            <input
+              className="appearance-none block w-full bg-blue-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="phone"
+              type="text"
+              placeholder="Phone"
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="message">
+              Message
+            </label>
+            <textarea
+              className=" no-resize appearance-none block w-full bg-blue-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-y"
+              id="message"
+            />
+          </div>
+        </div>
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3">
+            <button
+              className="shadow bg-ssblue focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="button"
+            >
+              Send
+            </button>
+          </div>
+          <div className="md:w-2/3" />
+        </div>
+      </form>
+    </>
+  </Layout>
+);
 
-export default Contact
+export default Contact;

639289f9f41421626b92cae6ac3ca8070f86aaec
Darryl Morley <darrylmorley.uk@gmail.com>
2021-02-26 11:46:59 +0000

    Add about page and correct link

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
           <h3 className="mb-2 text-white font-bold text-xl">About Us</h3>
           <ul className="text-white space-y-1">
             <li>
-              <Link href="/blog">
+              <Link href="/about">
                 <a className="hover:text-ssorange">
                   About Us
               </a>
@@ -138,104 +138,5 @@ const Footer = () => {
     </div>
   )
 }
-
-// const Footer = () => {
-//   return (
-//     <div className="w-full h-64 bg-gray-900">
-//       <div className="hidden lg:flex pb-8 pt-8 flex text-white justify-around border-t border-black mx-16">
-//         <div id="about-us-footer-menu">
-//           <h3 className="font-black text-xl">ABOUT US</h3>
-//           <hr className="mb-4 mt-1" />
-//           <ul>
-//             <li>
-//               <g-link to="/blog" className="hover:text-ssorange font-semibold text-lg">BLOG</g-link>
-//             </li>
-//             <li>
-//               <g-link to="/blog/about-us" className="hover:text-ssorange font-semibold text-lg">ABOUT US</g-link>
-//             </li>
-//             <li>
-//               <g-link
-//                 to="/blog/contact-us"
-//                 className="hover:text-ssorange font-semibold text-lg"
-//               >CONTACT US</g-link>
-//             </li>
-//             <li>
-//               <g-link
-//                 to="/blog/privacy-policy"
-//                 className="hover:text-ssorange font-semibold text-lg"
-//               >PRIVACY POLICY</g-link>
-//             </li>
-//           </ul>
-//         </div>
-//         <div id="products-footer-menu">
-//           <h3 className="font-black text-xl">PRODUCTS</h3>
-//           <hr className="mb-4 mt-1" />
-//           <ul>
-//             <li>
-//               <g-link to="/guns" className="hover:text-ssorange font-semibold text-lg">GUNS</g-link>
-//             </li>
-//             <li>
-//               <g-link to="/optics" className="hover:text-ssorange font-semibold text-lg">OPTICS</g-link>
-//             </li>
-//             <li>
-//               <g-link to="/accessories" className="hover:text-ssorange font-semibold text-lg">ACCESSORIES</g-link>
-//             </li>
-//             <li>
-//               <g-link to="/security" className="hover:text-ssorange font-semibold text-lg">SECURITY</g-link>
-//             </li>
-//           </ul>
-//         </div>
-//         <div id="social-footer-menu">
-//           <h3 className="font-black text-xl">FIND US ON</h3>
-//           <hr className="mb-4 mt-1" />
-//           <a
-//             href="https://www.facebook.com/ShootingSuppliesLTD/"
-//             target="_blank"
-//             className="hover:text-ssorange mr-4"
-//           >
-
-//           </a>
-
-//           <a
-//             href="https://twitter.com/shootingsupplys"
-//             target="_blank"
-//             className="hover:text-ssorange mr-4"
-//           >
-
-//           </a>
-
-//           <a
-//             href="https://www.instagram.com/shootingsupplies/"
-//             target="_blank"
-//             className="hover:text-ssorange mr-4"
-//           >
-
-//           </a>
-
-//           <a
-//             href="mailto:info@shootingsuppliesltd.co.uk"
-//             target="_blank"
-//             className="hover:text-ssorange mr-4"
-//           >
-
-//           </a>
-//         </div>
-
-//         <div id="contact-us-footer-menu">
-//           <h3 className="font-black text-xl">CONTACT US</h3>
-//           <hr className="mb-4 mt-1" />
-//           <p>38 SHERWOOD ROAD, BROMSGROVE, B60 3DR</p>
-//           <p>TEL: 01527 31 261</p>
-//           <p className="hover:text-ssorange">
-//             EMAIL:
-//           <a href="mailto:info@shootingsuppliesltd.co.uk"> info@shootingsuppliesltd.co.uk</a>
-//           </p>
-
-
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
 
 export default Footer
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,11 +1,46 @@
-import Layout from "../components/layout/Layout"
+import Image from 'next/image';
+import Layout from '../components/layout/Layout';
 
 export default function About() {
   return (
     <Layout>
-    <main className="mx-72 mt-24">
-      <h1 className="text-2xl font-black uppercase">About Us</h1>
-    </main>
+      <main className="mx-72 my-12">
+        <h1 className="text-3xl font-black uppercase">About Us</h1>
+        <div className="mt-4">
+          <Image
+            src="https://res.cloudinary.com/shooting-supplies/image/upload/v1573564805/shop/ShootingSuppliesStore_inkcxw_qmb2bo.jpg"
+            width={600}
+            height={280}
+            className="rounded-lg"
+          />
+        </div>
+        <p className="mt-4">
+          Shooting Supplies opened in the heart of Bromsgrove in 2002 and just four years later moved to its present
+          location offering a purpose-built shop featuring a dedicated gun room on the outskirts of the town.
+        </p>
+        <p className="mt-2">
+          The 3000 sq ft premises offers customers a huge selection of products in a convenient location, just off the
+          A38 and close to both the M5 and M42 motorways, providing plenty of parking.
+        </p>
+        <p className="mt-2">
+          Knowledgeable and experienced staff are always on hand to provide a helpful and friendly service to help
+          fulfil our number one aim, for customers to leave the shop happy with their purchases and confident in the
+          advice they have received. Our philosophy that ‘customer service is paramount’ is key to our business.
+        </p>
+        <p className="mt-2">
+          Shooting Supplies cater for all disciplines of shooting, offering all of the necessary accessories, enabling
+          customers to visit just one shop to fill all of their shooting needs.
+        </p>
+        <h2 className="mt-8 mb-4 font-bold text-lg uppercase">Have a look around our store with Google 360.</h2>
+        <iframe
+          title="ViewStore"
+          src="https://www.google.com/maps/embed?pb=!4v1566386400468!6m8!1m7!1sCAoSLEFGMVFpcE91bVBPZTVSZ3ZLNnpJM213QXppNmFZVTRqckZ1WmFZdWplWUhy!2m2!1d52.31852767!2d-2.06014551!3f20!4f10!5f0.7820865974627469"
+          width="600"
+          height="450"
+          frameBorder="0"
+          allowFullScreen=""
+        />
+      </main>
     </Layout>
-  )
+  );
 }

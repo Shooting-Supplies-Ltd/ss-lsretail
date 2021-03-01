6f723e57045f8710b89b8b5990a6180a69761374
Darryl Morley <darrylmorley.uk@gmail.com>
2021-03-01 15:37:30 +0000

    remove form for now

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -4,7 +4,40 @@ const Contact = () => (
   <Layout>
     <main className="mx-96 my-12">
       <h1 className="text-3xl font-black">Contact Us</h1>
-      <form className="mt-4">
+      <div className="my-8">
+        <p>
+          <strong>Tel: </strong>
+          <a href="tel:01527831261" className="hover:text-ssorange">
+            01527 831261
+          </a>
+        </p>
+        <p className="mt-2">
+          <strong>Email: </strong>
+          <a href="mailto:info@shootingsuppliesltd.co.uk" className="hover:text-ssorange">
+            info@shootingsuppliesltd.co.uk
+          </a>
+        </p>
+        <p>
+          <strong>Address: </strong>
+          <address className="mt-2 inline-block">38, Sherwood Road, Bromsgrove, B60 3DR</address>
+        </p>
+      </div>
+      <hr className="my-8" />
+      <div>
+        <h2 className="font-bold text-xl mb-2">Opening Times</h2>
+        <ul>
+          <li>Mon - Closed</li>
+          <li>Tue - 09:00 - 17:30</li>
+          <li>Wed - 09:00 - 17:30</li>
+          <li>Thu - 09:00 - 17:30</li>
+          <li>Fri - 09:00 - 19:00</li>
+          <li>Sat - 09:00 - 17:30</li>
+          <li>Sun - Closed</li>
+        </ul>
+      </div>
+      <hr className="my-8" />
+      {/* <h2 className="text-xl font-bold mb-8">Send Us a Message</h2> */}
+      {/* <form className="mt-4">
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label className="block uppercase tracking-wide text-gray-700 font-bold mb-2" htmlFor="first-name">
@@ -14,18 +47,18 @@ const Contact = () => (
               className="appearance-none block w-full bg-blue-100 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
               id="first-name"
               type="text"
-              placeholder="First"
+              placeholder="First Name"
             />
           </div>
           <div className="w-full md:w-1/2 px-3">
             <label className="block uppercase tracking-wide text-gray-700 font-bold mb-2" htmlFor="last-name">
-              Last Name
+              Surname
             </label>
             <input
               className="appearance-none block w-full bg-blue-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="last-name"
+              id="surname"
               type="text"
-              placeholder="Last"
+              placeholder="Surname"
             />
           </div>
         </div>
@@ -61,6 +94,7 @@ const Contact = () => (
             <textarea
               className=" no-resize appearance-none block w-full bg-blue-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-y"
               id="message"
+              placeholder="Message"
             />
           </div>
         </div>
@@ -75,7 +109,7 @@ const Contact = () => (
           </div>
           <div className="md:w-2/3" />
         </div>
-      </form>
+      </form> */}
     </main>
   </Layout>
 );

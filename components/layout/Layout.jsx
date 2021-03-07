7479fd2ae8b70311addf63ab54cfce8fbd373edc
Darryl Morley <darrylmorley.uk@gmail.com>
2021-03-07 11:49:59 +0000

    add site icons

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,40 +1,55 @@
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import NProgress from 'nprogress'
-import Head from 'next/head'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import NProgress from 'nprogress';
+import Head from 'next/head';
 
-import Header from './Header'
-import Footer from './Footer'
+import Header from './Header';
+import Footer from './Footer';
 
 NProgress.configure({ showSpinner: false });
 
 const Layout = (props) => {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      NProgress.start()
-    }
+      NProgress.start();
+    };
 
-    router.events.on('routeChangeStart', handleRouteChange)
-    router.events.on('routeChangeComplete', () => NProgress.done())
-    router.events.on('routeChangeError', () => NProgress.done())
-  }, [])
+    router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on('routeChangeComplete', () => NProgress.done());
+    router.events.on('routeChangeError', () => NProgress.done());
+  }, []);
 
   return (
     <div className="flex flex-col h-screen">
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/icons/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/icons/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Header />
-      <div className="flex-grow">
-        {props.children}
-      </div>
+      <div className="flex-grow">{props.children}</div>
       <div>
         <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;

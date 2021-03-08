5f4203d1e6f1b21e222b3945ea6f17380c570a19
Darryl Morley <darrylmorley.uk@gmail.com>
2021-03-08 13:46:52 +0000

    change stripe cancel url

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,23 @@
-import '../style/index.css'
-import ReactDOM from 'react-dom'
-import { loadStripe } from '@stripe/stripe-js'
-import { CartProvider } from 'use-shopping-cart'
+import '../style/index.css';
+import { loadStripe } from '@stripe/stripe-js';
+import { CartProvider } from 'use-shopping-cart';
 
 function MyApp({ Component, pageProps }) {
-  const stripePromise = loadStripe(process.env.STRIPE_API_PUBLIC_KEY)
+  const stripePromise = loadStripe(process.env.STRIPE_API_PUBLIC_KEY);
 
   return (
     <CartProvider
       stripe={stripePromise}
       mode="checkout-session"
       successUrl="stripe.com"
-      cancelUrl="twitter.com/dayhaysoos"
+      cancelUrl="shootingsuppliesltd.co.uk"
       currency="GBP"
       allowedCountries={['GB']}
-      billingAddressCollection={true}
+      billingAddressCollection
     >
       <Component {...pageProps} />
     </CartProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;

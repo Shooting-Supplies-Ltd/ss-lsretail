7c5387e4ec8e36cf970d2a0560c517548a24348f
Darryl Morley <darrylmorley.uk@gmail.com>
2021-03-09 14:33:48 +0000

    change stock message

--- components/StockMessage.jsx
+++ components/StockMessage.jsx
@@ -1,9 +1,11 @@
 export default function StockMessage() {
   return (
     <div className="xl:mt-4 p-4 bg-ssblue rounded-lg">
-      <p className="text-white font-semibold uppercase">Our stock is being added.</p>
+      <p className="text-white font-semibold uppercase">
+        We are currently adding all of our stock lines to the website, some items aren't yet listed.
+      </p>
       <p className="mt-2 text-white font-semibold uppercase">
-        If you can't find what your looking for, call us{' '}
+        If you can't find what your looking for, please call us{' '}
         <span className="mt-2 text-ssorange text-xl">
           <a href="tel:01527831261">01527831261</a>
         </span>

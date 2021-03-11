68827561a6b6db5a602de164f30dd0cf1b713573
Darryl Morley <darrylmorley.uk@gmail.com>
2021-03-11 12:10:01 +0000

    add next sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteUrl: 'https://shootingsuppliesltd.co.uk',
+  generateRobotsTxt: false, // (optional)
+  changefreq: 'daily',
+  priority: 0.7,
+  exclude: ['/api'],
+  transform: async (config, path) => ({
+    loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+    changefreq: config.changefreq,
+    priority: config.priority,
+    lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+  }),
+};

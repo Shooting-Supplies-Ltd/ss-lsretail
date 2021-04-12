1c99ccfca65cd7a85512a9094a423428ab6d9221
Darryl Morley <darrylmorley.uk@gmail.com>
2021-04-12 19:28:37 +0100

    update lightspeed product layout

--- components/LightspeedProduct.jsx
+++ components/LightspeedProduct.jsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { formatCurrencyString } from 'use-shopping-cart';
 
 export default function LightspeedProduct({ item }) {
+  console.log(item);
   const router = useRouter();
   const productDescriptionLong = () => ({ __html: item.ItemECommerce ? item.ItemECommerce.longDescription : '' });
   const productDescriptionShort = () => ({ __html: item.ItemECommerce ? item.ItemECommerce.shortDescription : '' });
@@ -69,43 +70,44 @@ export default function LightspeedProduct({ item }) {
               className="object-scale-down"
             />
           </div>
-          <div className="p-6 xl:w-1/2 p-2 bg-gray-50 rounded-lg">
-            <h1 className="text-4xl font-bold">{item.description}</h1>
-            <p>SKU: {item.customSku}</p>
-            <p className="mt-2 font-bold text-4xl uppercase">
+          <div className="p-4 xl:w-1/2 bg-gray-50 rounded-xl">
+            <h1 className="text-4xl font-black">{item.description}</h1>
+            <p className="mt-1">SKU: {item.customSku}</p>
+
+            <div>
+              {item.ItemShops ? (
+                item.ItemShops.ItemShop[0].qoh > 0 ? (
+                  <p className="mt-1 text-green-500 font-bold uppercase">In Stock</p>
+                ) : item.ItemShops.ItemShop[0].backorder >= 1 ? (
+                  <p className="mt-1 text-red-500 font-bold uppercase">Out of Stock - On Order</p>
+                ) : (
+                  <p className="mt-1 text-red-500 font-bold uppercase">Out of Stock</p>
+                )
+              ) : (
+                ''
+              )}
+            </div>
+
+            <p className="mt-4 font-bold text-4xl uppercase">
               {formatCurrencyString({
                 value: price,
                 currency: 'GBP',
               })}
             </p>
-            {item.ItemShops ? (
-              item.ItemShops.ItemShop[0].qoh > 0 ? (
-                <p className="mt-2 text-green-500 font-bold uppercase text-sm">In Stock</p>
-              ) : item.ItemShops.ItemShop[0].backorder >= 1 ? (
-                <p className="mt-2 text-red-500 font-bold uppercase text-sm">Out of Stock - On Order</p>
-              ) : (
-                <p className="mt-2 text-red-500 font-bold uppercase text-sm">Out of Stock</p>
-              )
-            ) : (
-              ''
-            )}
-            <p className="mt-4" dangerouslySetInnerHTML={productDescriptionShort()} />
-            <a href="#fulldescription" className="hidden xl:block">
-              <p className="mt-6 text-ssblue hover:text-ssorange">Full Description..</p>
-            </a>
+            <p className="mt-4 prose" dangerouslySetInnerHTML={productDescriptionShort()} />
           </div>
         </div>
-        <hr className="xl:hidden my-8" />
-        <div id="fulldescription" className="mx-0 xl:mx-72 mt-8 mb-12 p-6 bg-gray-50 rounded-lg">
-          <h2 className="font-bold text-2xl">Full Description</h2>
-          <div dangerouslySetInnerHTML={productDescriptionLong()} className="mt-4 prose" />
+        <hr className="xl:hidden my-4" />
+        <div id="fulldescription" className="mx-0 xl:mx-72 mt-4 mb-12 p-6 bg-gray-50 rounded-xl">
+          <h2 className="font-bold text-3xl text-center uppercase font-black">Description</h2>
+          <div dangerouslySetInnerHTML={productDescriptionLong()} className="mt-4 mx-auto prose" />
         </div>
         <div className="xl:mx-72 mx-4">
-          <h3 className="my-8 text-2xl font-black uppercase">
+          <h3 className="my-8 text-2xl font-black uppercase text-center">
             Please Contact Us to Purchase this item or for more information
           </h3>
         </div>
-        <div className="xl:mx-72 flex mx-4 my-8">
+        <div className="xl:mx-72 flex justify-center mx-4 my-8">
           <a
             href="tel:01527831261"
             className="flex items-center justify-center h-10 w-24 mr-4 bg-ssblue hover:bg-green-600 text-lg text-white font-bold uppercase rounded"

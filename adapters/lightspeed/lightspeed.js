f0bd4f83499c110a11e0dbefc81d526b13be1f6d
Darryl Morley <darrylmorley.uk@gmail.com>
2021-04-20 08:08:53 +0100

    add pellets to ammo

--- adapters/lightspeed/lightspeed.js
+++ adapters/lightspeed/lightspeed.js
@@ -82,7 +82,7 @@ export async function getItemsByCategory(categoryID) {
 
 export async function getAmmo() {
   const ammo = makeRequest(`Item.json?load_relations=["Category", "Images", "ItemShops", "ItemECommerce"]
-  &Category.categoryID=IN,[23,46,120,180, 218]&ItemECommerce.listOnStore=true&orderby=description`);
+  &Category.categoryID=IN,[23,46,120,180, 218, 71, 62, 116, 197, 35, 233]&ItemECommerce.listOnStore=true&orderby=description`);
   return ammo;
 }
 

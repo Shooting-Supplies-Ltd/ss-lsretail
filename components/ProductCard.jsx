0bd2b5fbedb884710305d40ac917ca6dcb09b7fb
Darryl Morley <darrylmorley.uk@gmail.com>
2021-05-21 10:22:00 +0100

    Add sale sticker & remove hover text colour

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -18,11 +18,30 @@ const ProductCard = ({ item }) => {
           <div className="flex justify-center h-48 overflow-hidden">
             <Image src={imageUrl} alt={`Image of ${name}`} width={240} height={240} />
           </div>
-          <div className="h-32 p-4 flex flex-col bg-ssblue text-white hover:text-ssorange rounded-b-lg">
+          <div className="h-32 p-4 flex flex-col bg-ssblue text-white rounded-b-lg">
             <h2 className="flex justify-center text-center uppercase">{name}</h2>
-            <p className="mt-2 flex justify-center font-bold text-lg ">
-              £{parseFloat(item.Prices.ItemPrice[0].amount).toFixed(2)}
-            </p>
+            <div className="mt-2 text-center font-bold text-lg">
+              {item.CustomFieldValues.CustomFieldValue.map((field) => {
+                if (field.customFieldID === '5' && field.value === 'true') {
+                  return (
+                    <>
+                      <p>
+                        <span className="line-through mr-2">
+                          £{parseFloat(item.Prices.ItemPrice[1].amount).toFixed(2)}
+                        </span>
+                        £{parseFloat(item.Prices.ItemPrice[0].amount).toFixed(2)}
+                      </p>
+                      <span className="relative z-10 top-2 left-36 font-semibold inline-block py-1 px-2 uppercase rounded-md text-white bg-red-600 uppercase last:mr-0 mr-1">
+                        Sale
+                      </span>
+                    </>
+                  );
+                }
+              })}
+              <p className="mt-2 flex justify-center font-bold text-lg ">
+                £{parseFloat(item.Prices.ItemPrice[0].amount).toFixed(2)}
+              </p>
+            </div>
           </div>
         </a>
       </Link>

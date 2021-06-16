4c3ebaacb9c4d1768ad81906fb4025e369c287be
Darryl Morley <darrylmorley.uk@gmail.com>
2021-06-16 16:43:31 +0100

    add g-analytics per next example

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,24 @@
 import '../style/index.css';
-import {useEffect} from 'react'
-import {useRouter} from 'next/router'
-import ReactGA from 'react-ga'
 import { loadStripe } from '@stripe/stripe-js';
 import { CartProvider } from 'use-shopping-cart';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 import Layout from '../components/layout/Layout';
 
 function MyApp({ Component, pageProps }) {
   const stripePromise = loadStripe(`${process.env.STRIPE_API_PUBLIC_KEY}`);
 
   const router = useRouter()
-
   useEffect(() => {
-    const handleRouteChange = (url, { shallow }) => {
-      ReactGA.set({ page: url })
-      ReactGA.pageview(url)
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
     }
-    ReactGA.initialize('G-10QXNWF6NZ', { debug: false })
-    ReactGA.set({ page: router.pathname })
-    ReactGA.pageview(router.pathname)
     router.events.on('routeChangeComplete', handleRouteChange)
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange)
     }
-  }, [])
+  }, [router.events])
 
   return (
     <CartProvider

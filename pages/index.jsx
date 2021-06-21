ef02338710b1d06b2ab6a2aa74587bd280e28671
Darryl Morley <darrylmorley.uk@gmail.com>
2021-06-21 08:18:45 +0100

    hide subheader

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,40 +1,43 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import HomeBanner from '../components/home/HomeBanner';
-import Categories from '../components/home/Categories';
-import HelpBanner from '../components/home/HelpBanner';
-import SubHeader from '../components/home/SubHeader';
+import HomeBanner from "../components/home/HomeBanner";
+import Categories from "../components/home/Categories";
+import HelpBanner from "../components/home/HelpBanner";
+import SubHeader from "../components/home/SubHeader";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>The Midlands Leading Supplier for Guns, Ammo, Accessories, Scopes, Clothing & More | Shooting Supplies Ltd</title>
+        <title>
+          The Midlands Leading Supplier for Guns, Ammo, Accessories, Scopes,
+          Clothing & More | Shooting Supplies Ltd
+        </title>
         <link rel="canonical" href="https://www.shootingsuppliesltd.co.uk" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
-              '@context': 'http://schema.org',
-              '@type': 'LocalBusiness',
-              name: 'Shooting Supplies Ltd',
+              "@context": "http://schema.org",
+              "@type": "LocalBusiness",
+              name: "Shooting Supplies Ltd",
               description:
-                'The Midlands Leading Supplier for Guns, Ammo, Accessories, Scopes, Clothing & More.',
-              openingHours: ['Tu,We,Th,Sa 09:00-17:30', 'Fr 09:00-19:00'],
-              telephone: '01527831261',
-              paymentAccepted: 'Cash, Credit Card, Debit Card, Finance',
-              url: 'https://www.shootingsuppliesltd.co.uk',
+                "The Midlands Leading Supplier for Guns, Ammo, Accessories, Scopes, Clothing & More.",
+              openingHours: ["Tu,We,Th,Sa 09:00-17:30", "Fr 09:00-19:00"],
+              telephone: "01527831261",
+              paymentAccepted: "Cash, Credit Card, Debit Card, Finance",
+              url: "https://www.shootingsuppliesltd.co.uk",
               address: {
-                '@type': 'PostalAddress',
-                streetAddress: '38, Sherwood Road',
-                addressLocality: 'Bromsgrove',
-                addressRegion: 'Worcestershire',
-                postalCode: 'B60 3DR',
-                email: 'info@shootingsuppliesltd.co.uk',
+                "@type": "PostalAddress",
+                streetAddress: "38, Sherwood Road",
+                addressLocality: "Bromsgrove",
+                addressRegion: "Worcestershire",
+                postalCode: "B60 3DR",
+                email: "info@shootingsuppliesltd.co.uk",
                 image:
-                  'https://res.cloudinary.com/shooting-supplies/image/upload/v1573564805/shop/ShootingSuppliesStore_inkcxw_qmb2bo.jpg',
+                  "https://res.cloudinary.com/shooting-supplies/image/upload/v1573564805/shop/ShootingSuppliesStore_inkcxw_qmb2bo.jpg",
               },
-              logo: '/logos/Logo.webp',
+              logo: "/logos/Logo.webp",
             }),
           }}
         />
@@ -46,9 +49,15 @@ export default function Home() {
           name="keywords"
           content="shotgun, air rifle, air pistol, long barrel pistol, firearms, rifle, rifle scope, night vision, binoculars, spotters, bipods, foregrip, pellets, air gun pellets, gun mounts, rangefinder, thermal scope"
         />
-        <meta property="og:url" content="https://www.shootingsuppliesltd.co.uk" />
+        <meta
+          property="og:url"
+          content="https://www.shootingsuppliesltd.co.uk"
+        />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Shooting Supplies Ltd | The Midlands leading gun shop." />
+        <meta
+          property="og:title"
+          content="Shooting Supplies Ltd | The Midlands leading gun shop."
+        />
         <meta
           property="og:image"
           content="https://res.cloudinary.com/shooting-supplies/image/upload/v1573564805/shop/ShootingSuppliesStore_inkcxw_qmb2bo.jpg"
@@ -62,9 +71,9 @@ export default function Home() {
         <div className="hidden xl:block">
           <HomeBanner />
         </div>
-        <div className="hidden xl:block">
+        {/* <div className="hidden xl:block">
           <SubHeader />
-        </div>
+        </div> */}
         <div>
           <Categories />
         </div>

25d4d62675b9352bc3fd7404c53b2999bcbc7983
Darryl Morley <darrylmorley.uk@gmail.com>
2021-07-01 12:18:05 +0100

    move lsProduct Head to component

--- components/product-page/LightspeedProduct.jsx
+++ components/product-page/LightspeedProduct.jsx
@@ -1,69 +1,26 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { formatCurrencyString } from 'use-shopping-cart';
+import Image from "next/image";
+import LsProductHead from "../seo/LightspeedProduct";
+import MailTo from "./MailTo";
+import { useRouter } from "next/router";
+import { formatCurrencyString } from "use-shopping-cart";
 
 export default function LightspeedProduct({ item }) {
   const router = useRouter();
-  const productDescriptionLong = () => ({ __html: item.ItemECommerce ? item.ItemECommerce.longDescription : '' });
-  const productDescriptionShort = () => ({ __html: item.ItemECommerce ? item.ItemECommerce.shortDescription : '' });
-  const price = parseFloat(item.Prices?.ItemPrice[0]?.amount).toFixed(2).replace('.', '');
+  const productDescriptionLong = () => ({
+    __html: item.ItemECommerce ? item.ItemECommerce.longDescription : "",
+  });
 
-  const Mailto = ({ email, subject = '', body = '', children }) => {
-    let params = subject || body ? '?' : '';
-    if (subject) params += `subject=${encodeURIComponent(subject)}`;
-    if (body) params += `${subject ? '&' : ''}body=${encodeURIComponent(body)}`;
+  const productDescriptionShort = () => ({
+    __html: item.ItemECommerce ? item.ItemECommerce.shortDescription : "",
+  });
 
-    return <a href={`mailto:${email}${params}`}>{children}</a>;
-  };
+  const price = parseFloat(item.Prices?.ItemPrice[0]?.amount)
+    .toFixed(2)
+    .replace(".", "");
 
   return (
     <>
-      <Head>
-      <title>{`${item.description} | Shooting Supplies Ltd`}</title>
-    <meta property="description" content={`${item.ItemECommerce.shortDescription.replace(/(<([^>]+)>)/gi, '')}`} />
-    <meta name="robots" content="index, follow" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="keywords" content={`${item.description.replace(' ', ',')}`} />
-    <meta property="og:title" content={`${item.description}`} />
-    <meta
-      property="og:description"
-      content={`${item.ItemECommerce.shortDescription.replace(/(<([^>]+)>)/gi, '')}`}
-    />
-    <meta
-      property="og:image"
-      content={`${item.Images.Image.baseImageURL}/w_600/${item.Images.Image.publicID}.webp`}
-      alt={`${item.description}`}
-    />
-    <meta property="og:url" content={`https://shootingsuppliesltd.co.uk${router.asPath}`} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta charSet="UTF-8" />
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{
-        __html: JSON.stringify({
-          '@context': 'http://schema.org',
-          '@type': 'Product',
-          sku: item.customSku,
-          image: `${item.Images.Image.baseImageURL}/w_600/${item.Images.Image.publicID}.webp`,
-          name: item.description,
-          brand: item.Manufacturer.name ? item.Manufacturer.name : '',
-          description: item.ItemECommerce.shortDescription.replace(/(<([^>]+)>)/gi, ''),
-          mpn: item.manufacturerSku ? item.manufacturerSku : '',
-          offers: {
-            priceValidUntil: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
-            '@type': 'Offer',
-            url: `https://shootingsuppliesltd.co.uk${router.asPath}`,
-            priceCurrency: 'GBP',
-            price: item.Prices?.ItemPrice[0]?.amount,
-            itemCondition: 'https://schema.org/NewCondition',
-            availability:
-              item.ItemShops?.ItemShop[0]?.qoh > 0 ? 'https://schema.org/InStock' : 'https://schema.org/OutOfStock',
-          },
-        }),
-      }}
-    />
-      </Head>
+      <LsProductHead item={item} />
       <main>
         <div className="lg:mx-72 lg:my-12 flex flex-col lg:flex-row justify-center">
           <div className="relative w-3/4 mx-auto lg:mx-0 lg:w-1/2 p-2">
@@ -81,29 +38,38 @@ export default function LightspeedProduct({ item }) {
             <div>
               {item.ItemShops ? (
                 item.ItemShops.ItemShop[0].qoh > 0 ? (
-                  <p className="mt-1 text-green-500 font-bold uppercase">In Stock</p>
+                  <p className="mt-1 text-green-500 font-bold uppercase">
+                    In Stock
+                  </p>
                 ) : item.ItemShops.ItemShop[0].backorder >= 1 ? (
-                  <p className="mt-1 text-red-500 font-bold uppercase">Out of Stock - On Order</p>
+                  <p className="mt-1 text-red-500 font-bold uppercase">
+                    Out of Stock - On Order
+                  </p>
                 ) : (
-                  <p className="mt-1 text-red-500 font-bold uppercase">Out of Stock</p>
+                  <p className="mt-1 text-red-500 font-bold uppercase">
+                    Out of Stock
+                  </p>
                 )
               ) : (
-                ''
+                ""
               )}
             </div>
 
             {item.CustomFieldValues ? (
               item.CustomFieldValues.CustomFieldValue.map((field) => {
-                if (field.customFieldID === '5' && field.value === 'true') {
+                if (field.customFieldID === "5" && field.value === "true") {
                   return (
                     <>
                       <p className="mt-4 font-bold text-4xl uppercase">
                         <span className="line-through mr-2 text-sm lg:text-md xl:text-xl">
-                          £{parseFloat(item.Prices.ItemPrice[1].amount).toFixed(2)}
+                          £
+                          {parseFloat(item.Prices.ItemPrice[1].amount).toFixed(
+                            2
+                          )}
                         </span>
                         {formatCurrencyString({
                           value: price,
-                          currency: 'GBP',
+                          currency: "GBP",
                         })}
                       </p>
                       <span className="absolute z-10 top-32 lg:top-44 xl:top-32 md:left-auto lg:left-60 xl:left-80 font-semibold inline-block py-2 px-3 rounded-lg md:text-2xl text-white bg-red-600 uppercase">
@@ -117,20 +83,20 @@ export default function LightspeedProduct({ item }) {
               <p className="mt-4 font-bold text-4xl uppercase">
                 {formatCurrencyString({
                   value: price,
-                  currency: 'GBP',
+                  currency: "GBP",
                 })}
               </p>
             )}
 
             {item.CustomFieldValues
               ? item.CustomFieldValues.CustomFieldValue.map((field) => {
-                  if (field.customFieldID === '5' && field.value === 'false') {
+                  if (field.customFieldID === "5" && field.value === "false") {
                     return (
                       <>
                         <p className="mt-4 font-bold text-4xl uppercase">
                           {formatCurrencyString({
                             value: price,
-                            currency: 'GBP',
+                            currency: "GBP",
                           })}
                         </p>
                       </>
@@ -139,13 +105,24 @@ export default function LightspeedProduct({ item }) {
                 })
               : null}
 
-            <p className="mt-4 prose" dangerouslySetInnerHTML={productDescriptionShort()} />
+            <p
+              className="mt-4 prose"
+              dangerouslySetInnerHTML={productDescriptionShort()}
+            />
           </div>
         </div>
         <hr className="lg:hidden my-4" />
-        <div id="fulldescription" className="mx-0 lg:mx-72 mt-4 mb-12 p-4 bg-gray-50 rounded-xl">
-          <h2 className="text-3xl text-center uppercase font-black">Description</h2>
-          <div dangerouslySetInnerHTML={productDescriptionLong()} className="mt-4 mx-auto prose" />
+        <div
+          id="fulldescription"
+          className="mx-0 lg:mx-72 mt-4 mb-12 p-4 bg-gray-50 rounded-xl"
+        >
+          <h2 className="text-3xl text-center uppercase font-black">
+            Description
+          </h2>
+          <div
+            className="mt-4 mx-auto prose"
+            dangerouslySetInnerHTML={productDescriptionLong()}
+          />
         </div>
         <div className="lg:mx-72 mx-4">
           <h3 className="my-8 text-2xl font-black uppercase text-center">
@@ -159,14 +136,14 @@ export default function LightspeedProduct({ item }) {
           >
             Call Us
           </a>
-          <Mailto
+          <MailTo
             email="info@shootingsuppliesltd.co.uk"
             subject={`ITEM ENQUIRY: ${item.description} / ${item.customSku}`}
           >
             <p className="flex items-center justify-center h-10 w-24 mr-4 bg-ssblue hover:bg-green-600 text-lg text-white font-bold uppercase rounded">
               Email
             </p>
-          </Mailto>
+          </MailTo>
         </div>
       </main>
     </>
---
+++ components/product-page/SeoHead.jsx
@@ -0,0 +1,73 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+export default function SeoHead({ item }) {
+  const router = useRouter();
+
+  return (
+    <Head>
+      <title>{`${item.description} | Shooting Supplies Ltd`}</title>
+      <meta
+        property="description"
+        content={`${item.ItemECommerce.shortDescription.replace(
+          /(<([^>]+)>)/gi,
+          ""
+        )}`}
+      />
+      <meta name="robots" content="index, follow" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="keywords" content={`${item.description.replace(" ", ",")}`} />
+      <meta property="og:title" content={`${item.description}`} />
+      <meta
+        property="og:description"
+        content={`${item.ItemECommerce.shortDescription.replace(
+          /(<([^>]+)>)/gi,
+          ""
+        )}`}
+      />
+      <meta
+        property="og:image"
+        content={`${item.Images.Image.baseImageURL}/w_600/${item.Images.Image.publicID}.webp`}
+        alt={`${item.description}`}
+      />
+      <meta
+        property="og:url"
+        content={`https://shootingsuppliesltd.co.uk${router.asPath}`}
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta charSet="UTF-8" />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "http://schema.org",
+            "@type": "Product",
+            sku: item.customSku,
+            image: `${item.Images.Image.baseImageURL}/w_600/${item.Images.Image.publicID}.webp`,
+            name: item.description,
+            brand: item.Manufacturer.name ? item.Manufacturer.name : "",
+            description: item.ItemECommerce.shortDescription.replace(
+              /(<([^>]+)>)/gi,
+              ""
+            ),
+            mpn: item.manufacturerSku ? item.manufacturerSku : "",
+            offers: {
+              priceValidUntil: new Date(
+                new Date().setFullYear(new Date().getFullYear() + 1)
+              ),
+              "@type": "Offer",
+              url: `https://shootingsuppliesltd.co.uk${router.asPath}`,
+              priceCurrency: "GBP",
+              price: item.Prices?.ItemPrice[0]?.amount,
+              itemCondition: "https://schema.org/NewCondition",
+              availability:
+                item.ItemShops?.ItemShop[0]?.qoh > 0
+                  ? "https://schema.org/InStock"
+                  : "https://schema.org/OutOfStock",
+            },
+          }),
+        }}
+      />
+    </Head>
+  );
+}
